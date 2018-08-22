bbdcd5cc1ba79e0ce12dfa3245395f0f41c1ab63
jerrychen44 <jerrychen040@gmail.com>
2018-08-22 19:39:55 +0800

    use temp val to rotate 90 degree

---
+++ Ch01.Arrays And Strings/7.Rotate_matrix/7.j_rotate_matrix.cpp
@@ -0,0 +1,144 @@
+/*
+ * Cracking the coding interview edition 6
+ * Problem 1.7 Rotate a matrix by 90' clockwise ( or anticlockwise).
+ * Solution : I have done it two ways.
+ * Approach 1: Take transpose of matrix and then reverse the rows for clockwise 90' rotation.
+ * 			   Obviously if we reverse the columns we will get anticlockwise 90' rotation.
+ * Approach 2: As mentioned in the book, rotating invididual elements layer by layer.
+ * 			   I have solved it perform anticlockwise 90' rotation, it can be done similarly for clockwise rotatation.
+ */
+#include<iostream>
+
+void printMatrix( int ** matrix, int N) {
+	for ( int i = 0; i < N; ++i ) {
+		for( int j = 0; j < N; ++j ) {
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void helper_transpose(int **matrix, int N)
+{
+	//swap the non-diagonal element
+	for( int i = 0; i < N; ++i ) {
+		for( int j = i+1; j < N; ++j ) {
+			if ( i != j ) {
+				std::swap(matrix[i][j], matrix[j][i]);
+			}
+		}
+	}
+}
+
+void helper_reverse( int * row, int N ) {
+	//swap head <-> tail, one row
+	for ( int i = 0; i < N/2; ++i ) {
+		std::swap(row[i], row[N-i-1]);
+	}
+}
+
+void rotate90_clockwise(int ** matrix, int N) {
+	//transpose matrix
+	helper_transpose(matrix, N);
+	//std::cout << "helper_transpose\n" << std::endl;
+	//printMatrix(matrix, N);
+
+	// reverse each row
+	for ( int i = 0; i < N; ++i ) {
+		helper_reverse(matrix[i], N);
+	}
+}
+
+void rotate90_clockwise_use_temp( int ** matrix, int N ) {
+	for( int i = 0; i < N/2; ++i ) {
+		for( int j = i; j < N-i-1; ++j ) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[N-j-1][i];
+				matrix[N-j-1][i] = matrix[N-i-1][N-j-1];
+				matrix[N-i-1][N-j-1]= matrix[j][N-i-1];
+				matrix[j][N-i-1] = temp;
+		}
+	}
+}
+
+
+
+void rotate90_anticlockwise( int ** matrix, int N ) {
+	for( int i = 0; i < N/2; ++i ) {
+		for( int j = i; j < N-i-1; ++j ) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[j][N-i-1];
+				matrix[j][N-i-1] = matrix[N-i-1][N-j-1];
+				matrix[N-i-1][N-j-1]= matrix[N-j-1][i];
+				matrix[N-j-1][i] = temp;
+		}
+	}
+}
+
+
+
+
+int main() {
+	/*
+	int N;
+	std::cout << "Enter N for NxN matrix:";
+	std::cin >> N;
+	int ** matrix = new int*[N];
+	for ( int i = 0; i < N; ++i ) {
+		matrix[i] = new int[N];
+	}
+
+	for ( int i = 0; i < N; ++i) {
+		for ( int j = 0; j < N; ++j ) {
+			std::cin >> matrix[i][j];
+		}
+	}
+	*/
+	int N = 4;//4x4
+	//Matrix init
+	int ** matrix = new int*[N];
+	for ( int i = 0; i < N; ++i ) {
+		matrix[i] = new int[N];
+	}
+	matrix[0][0] = 0;
+	matrix[0][1] = 1;
+	matrix[0][2] = 2;
+	matrix[0][3] = 3;
+
+	matrix[1][0] = 4;
+	matrix[1][1] = 5;
+	matrix[1][2] = 6;
+	matrix[1][3] = 7;
+
+	matrix[2][0] = 8;
+	matrix[2][1] = 9;
+	matrix[2][2] = 10;
+	matrix[2][3] = 11;
+
+	matrix[3][0] = 12;
+	matrix[3][1] = 13;
+	matrix[3][2] = 14;
+	matrix[3][3] = 15;
+
+	printMatrix(matrix, N);
+
+
+	std::cout << "Rotated matrix by 90 (clockwise by temp value):\n";
+	rotate90_clockwise_use_temp(matrix, N);
+	std::cout << std::endl;
+	printMatrix(matrix, N);
+
+
+
+	std::cout << "Rotated matrix by 90 (clockwise):\n";
+	rotate90_clockwise(matrix, N);
+	std::cout << std::endl;
+	printMatrix(matrix, N);
+
+
+
+	std::cout << "Rotated matrix again by 90(anticlockwise):\n";
+	rotate90_anticlockwise(matrix, N);
+	printMatrix(matrix, N);
+	return 0;
+}
